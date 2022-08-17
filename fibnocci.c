22ba08f986c3b298bbda3e6d518a3a17f1a46e5d
sowmyasrisalendram <109757014+sowmyasrisalendram@users.noreply.github.com>
2022-08-17 21:53:16 +0530

    Add files via upload

--- fibnocci.c
+++ fibnocci.c
@@ -7,6 +7,8 @@ int main()
         scanf("%d",&n);
         fib[0] = 0;
         fib[1] = 1;
-            for (i = 0; i < n; i++)
-                printf("%3d   %6d\n", i, fib[i]);
+         for(i = 2; i < n; i++)
+                fib[i] = fib[i-1] + fib[i-2];
+         for (i = 0; i < n; i++)
+                printf("%d   %d\n", i, fib[i]);
 }
