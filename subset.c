762b0d08eee78f8cfebd8cb5bd802eb1a3ae2f83
sowmyasrisalendram <109757014+sowmyasrisalendram@users.noreply.github.com>
2022-08-27 22:45:45 +0530

    Add files via upload

---
+++ subset.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+#include <string.h> 
+ 
+int main()
+ { 
+    char st[]='', ch; 
+    scanf("%d",&ch);
+    int l = strlen(st), i, j, k, count = 0; 
+     
+    for (i = 0; i < l; i++)
+	 { 
+        for (j = 0; j < l-i; j++) 
+		{ 
+            for (k = j; k < j+i+1; k++) 
+			{ 
+                printf("%c", st[k]); 
+            } 
+            printf("\n"); 
+        } 
+    } 
+    return 0; 
+}
